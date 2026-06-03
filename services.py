f289a012e9e9a1712d4348d0e148e602e8234e42
milind dinesh <milinddinesh@gmail.com>
2026-06-04 01:19:52 +0530

    add rot cipher

--- services.py
+++ services.py
@@ -2,22 +2,6 @@
 
 # ---- Begin Replacement Ciphers --- 
 
-# def replacement_cipher_encrypt(plaintext, key , replacment_text):
-    # plaintext : the original string 
-    # key : the substring to look for 
-    # replacement_text : the substring used to replace the key string 
-    # both the key and replacement_text can be of length between 1 and len(plaintex)
-    # returns encoded string 
-
-    #Theoretically, we can use this function to replace any substring with any other substring
-
-    # left = 0 = len(replacment_text)
-    # right = 0
-    # while (left < len(replacment_text) and right < len(replacment_text)):
-        
-#Mono character replacement
-
-#How does the function perfom when a large string is given as input ?
 def replace_character(plaintext, key, replacement_character):
     #plaintext : the original string
     #key : the character to look for
@@ -36,8 +20,40 @@ def replace_character(plaintext, key, replacement_character):
             encoded_string += character
     return encoded_string
 
+# Ceaser cipher , rot13 cipher
+def rotate_character(plaintext, key):
+    #plaintext : the original string
+    #key : the number of positions to rotate each character
+    #returns encoded string
+    #uses the whole ascii table
+
+    encoded_string = ""
+    for character in plaintext:
+        if character == " ":
+            encoded_string += " "
+        else:
+            encoded_string += chr((ord(character) + key) % 256)
+    return encoded_string
+
 if __name__ == "__main__":
     plaintext = "hello world"
-    key = "o"
-    replacement_character = "x"
-    print(replace_character(plaintext, key, replacement_character))
+    key = 13
+    print(rotate_character(plaintext, key))
+
+
+# def replacement_cipher_encrypt(plaintext, key , replacment_text):
+    # plaintext : the original string 
+    # key : the substring to look for 
+    # replacement_text : the substring used to replace the key string 
+    # both the key and replacement_text can be of length between 1 and len(plaintex)
+    # returns encoded string 
+
+    #Theoretically, we can use this function to replace any substring with any other substring
+
+    # left = 0 = len(replacment_text)
+    # right = 0
+    # while (left < len(replacment_text) and right < len(replacment_text)):
+        
+#Mono character replacement
+
+#How does the function perfom when a large string is given as input ?
